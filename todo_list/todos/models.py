97480654d2720fa19be607403592b2350671c8fb
DillonTodd <98577108+DillonTodd@users.noreply.github.com>
2022-05-25 11:39:33 -0500

    add Todo model and migration

--- todo_list/todos/models.py
+++ todo_list/todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title

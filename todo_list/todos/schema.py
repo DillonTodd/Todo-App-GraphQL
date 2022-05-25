d19c90aaaa5abea8620055f6226ddcd5d8abbe3d
DillonTodd <98577108+DillonTodd@users.noreply.github.com>
2022-05-25 12:08:35 -0500

    add the create, update, delete mutations to the schema

--- todo_list/todos/schema.py
+++ todo_list/todos/schema.py
@@ -19,4 +19,71 @@ class Query(graphene.ObjectType):
         return Todo.objects.get(id=todo_id)
 
 
-schema = graphene.Schema(query=Query)
+class TodoInput(graphene.InputObjectType):
+    title = graphene.String()
+    description = graphene.String()
+    completed = graphene.Boolean()
+
+
+class CreateTodoMutation(graphene.Mutation):
+    class Arguments:
+        input = TodoInput(required=True)
+
+    todo = graphene.Field(TodoType)
+
+    @classmethod
+    def mutate(cls, root, info, input):
+        todo_instance = Todo(
+            title = input.title,
+            description = input.description,
+            completed = input.completed
+        )
+
+        todo_instance.save()
+
+        return CreateTodoMutation(todo=todo_instance)
+
+
+class UpdateTodoMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.String()
+        input = TodoInput(required=True)
+
+    todo = graphene.Field(TodoType)
+
+    @classmethod
+    def mutate(cls, root, info, input, id):
+        todo_instance = Todo.objects.get(id=id)
+
+        if todo_instance:
+            todo_instance.title = input.title
+            todo_instance.description = input.description
+            todo_instance.completed = input.completed
+
+        todo_instance.save()
+
+        return UpdateTodoMutation(todo=todo_instance)
+
+
+class DeleteTodoMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.String()
+        input = TodoInput(required=True)
+
+    todo = graphene.Field(TodoType)
+
+    @classmethod
+    def mutate(cls, root, info, input, id):
+        todo_instance = Todo.objects.get(id=id)
+        todo_instance.delete()
+
+        return DeleteTodoMutation(todo=todo_instance)
+
+
+class Mutation(graphene.ObjectType):
+    create_todo_mutation = CreateTodoMutation.Field()
+    update_todo_mutation = UpdateTodoMutation.Field()
+    delete_todo_mutation = DeleteTodoMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
